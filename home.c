2845add1c62d11107f1b70f8bdfce40a10b28576
Bartek <bartoszluka1@gmail.com>
2020-12-16 11:39:19 +0100

    easier writing to file and some garbage added

--- home.c
+++ home.c
@@ -1,55 +1,161 @@
 #include "list.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
 void saveMatrixToFile(List *vertices[], int listSize, char *path);
 void freeMatrix(List *vertices[], int n);
-void openSave(List *vertices[], int *n, char *path);
+List **openSave(int *n, char *path);
+void writeToFile(List **vertices, int m, char *path);
+void readFromFile(char *path);
+off_t fsize(const char *filename)
+{
+    struct stat st;
+
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+
+    return -1;
+}
+
 int main(int argc, char *argv[])
 {
 
     char *path = "./save.txt";
     int n = 5;
     // scanf("%d", &n);
-    List **vertices = (List **)malloc(sizeof(List *) * n);
-
-    openSave(NULL, NULL, path);
+    List **vertices; // = openSave(NULL, path);
+    vertices = (List **)malloc(sizeof(List *) * n);
+    for (size_t i = 0; i < n; i++)
+    {
+        vertices[i] = newList();
+        addIntsToList2(vertices[i], 0);
+    }
 
     if (!vertices)
     {
+
         ERR("malloc");
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        vertices[i] = newList();
-    }
-
-    addIntsToList2(vertices[1], 2);
-    addIntsToList2(vertices[1], 3);
-    addIntsToList2(vertices[1], 0);
-
-    saveMatrixToFile(vertices, n, path);
-
+    writeToFile(vertices, n, "save2");
+    // saveMatrixToFile(vertices, n, path);
+    // readFromFile("save2");
     freeMatrix(vertices, n);
 
     return 0;
 }
-
-void openSave(List *vertices[], int *n, char *path)
+void readFromFile(char *path)
 {
+    FILE *infile;
+    List **vertices;
+
+    infile = fopen(path, "r");
+    if (infile == NULL)
+    {
+        ERR("fopen");
+    }
+
+    // read file contents till end of file
+    fread(*vertices, sizeof(List *), 1, infile);
+
+    // close file
+    fclose(infile);
+}
+
+void writeToFile(List **vertices, int m, char *path)
+{
+    FILE *outfile;
+    outfile = fopen(path, "w");
+    if (outfile == NULL)
+    {
+        ERR("fopen");
+    }
+
+    fprintf(outfile, "%d;\n", m);
+    // write struct to file
+    for (int i = 0; i < m; i++)
+    {
+        fprintf(outfile, "%d:", i);
+        Node *p = vertices[i]->head;
+        while (p)
+        {
+            fprintf(outfile, "%d ", *(int *)(p->data));
+
+            p = p->next;
+        }
+        fprintf(outfile, "\n");
+    }
+
+    // close file
+    fclose(outfile);
+}
+
+List **openSave(int *n, char *path)
+{
+    List **vertices;
     int fd;
     int m;
+    int filesize = fsize(path);
+    char *buff = (char *)malloc(sizeof(char) * filesize);
+
     if ((fd = open(path, O_RDONLY)) < 0)
         ERR("open");
-    char buff[20];
-    // while (1)
-    // {
-    if (read(fd, buff, 3) < 0)
+    char text[3];
+
+    //zmienic na bulk_read/obrone przed sygnalami
+    if (read(fd, buff, filesize) < 0)
     {
         ERR("read");
     }
-    m = atoi(buff);
-    printf("%d\n", m);
+    char tmp[2];
+    for (int i = 0; i < filesize; i++)
+    {
+        switch (buff[i])
+        {
+        case ';':
+            m = atoi(text);
+            vertices = (List **)malloc(sizeof(List *) * m);
+
+            break;
+        case '\n':
+            printf("%s\n", text);
+            strcpy(text, "");
+            break;
+        case ':':
+            m = atoi(text);
+            vertices[m] = newList();
+            break;
+        default:
+            sprintf(tmp, "%c", buff[i]);
+            strcat(text, tmp);
+            break;
+        }
+    }
+
+    // for (int i = 0; i < m; i++)
+    // {
+    //     if (read(fd, buff, 1) < 0)
+    //     {
+    //         ERR("read");
+    //     }
+    //     switch (buff[0])
+    //     {
+    //     case ':':
+
+    //         break;
+    //     case '\n':
+    //         printf("%s\n", text);
+    //         strcpy(text, "");
+    //         break;
+    //     default:
+    //         strcpy(text, buff);
+    //         break;
+    //     }
+    //     // vertices[i] = newList();
     // }
+    free(buff);
     close(fd);
+    return vertices;
 }
 
 void freeMatrix(List *vertices[], int n)
