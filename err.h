9c7002ce6bb754ca67a2c88e2b1a434eb898b29a
Bartek <bartoszluka1@gmail.com>
2020-12-27 16:22:37 +0100

    added err.h

---
+++ err.h
@@ -0,0 +1,10 @@
+#ifndef ERR_H_
+#define ERR_H_
+#include <signal.h>
+#include <errno.h>
+
+#define ERR(source) (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), \
+                     perror(source), kill(0, SIGKILL),               \
+                     exit(EXIT_FAILURE))
+
+#endif
--- list.h
+++ list.h
@@ -10,10 +10,6 @@
 #include <time.h>
 #include <string.h>
 
-#define ERR(source) (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), \
-                     perror(source), kill(0, SIGKILL),               \
-                     exit(EXIT_FAILURE))
-
 typedef struct Node
 {
     void *data;
